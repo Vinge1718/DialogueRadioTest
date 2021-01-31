fd953a19e7bdec624f49d99d2b5c7ad19dece1d0
Vinge1718 <vinge1718@gmail.com>
2021-01-31 20:21:07 +0300

    added a builder to the project

--- app/src/main/java/com/example/dialoguetest/MoodDialogFragment.java
+++ app/src/main/java/com/example/dialoguetest/MoodDialogFragment.java
@@ -1,46 +1,40 @@
 package com.example.dialoguetest;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import java.util.Objects;
 
 public class MoodDialogFragment extends DialogFragment {
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        Objects.requireNonNull(getDialog()).setTitle("Simple Dialog");
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+        builder.setTitle("Dialog via Builder");
+        builder.setMessage("Would you like to take a survey?");
 
-        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
-        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(DialogInterface dialog, int which) {
                 dismiss();
             }
         });
 
-        RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup); //pull group
-        int selectedId = surveyRadioGroup.getCheckedRadioButtonId(); //get selected ID
-        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId); //get r button val via ID
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Log.d("testing", selectedRadioButton.getText().toString());
+            public void onClick(DialogInterface dialog, int which) {
                 dismiss();
             }
         });
-
-        return rootView;
+        return builder.create();
     }
 }
